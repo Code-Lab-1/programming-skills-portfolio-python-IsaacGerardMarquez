4b374abf2d61a6e8414d50b357b83ea58299e866
Isaac Gerard <IsaacBathSpa@gmail.com>
2024-09-20 09:11:56 +0400

    Exercise 01 - Coding is Cool.py

---
+++ Exercise 01 - Coding is Cool/Exercise 01 - Coding is Cool.py
@@ -0,0 +1,10 @@
+## Exercise 1: Coding is Cool- 10 Marks
+
+# Fill in the blanks in the Python code below to output the phrase **"Coding is Cool"** to the console using variables and string concatenation.
+
+word1 = "Coding"
+word2 = "is"
+word3 = "Cool"
+
+# String Concatenation
+print(word1 + " " + word2 + " " + word3)
